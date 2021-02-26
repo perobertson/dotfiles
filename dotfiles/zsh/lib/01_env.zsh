52d26c6111e4f2b75d2055d20c1f427ba807bacb
Paul Robertson <t.paulrobertson@gmail.com>
2021-02-25 21:43:04 -0500

    zsh: enable scrolling when using less as the pager

--- dotfiles/zsh/lib/01_env.zsh
+++ dotfiles/zsh/lib/01_env.zsh
@@ -2,3 +2,8 @@ export EDITOR='vim'
 export CODE_PATH="$HOME/workspace"
 GPG_TTY=$(tty)
 export GPG_TTY
+
+# Configure less as the pager and reprint the screen when content changes
+# Enables scrolling
+export PAGER=less
+export LESS=-R
